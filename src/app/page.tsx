d8e388723fac30718e16f460e7c082fcac52ec3c
mostafarawhy <aboelznog@gmail.com>
2026-03-11 21:24:27 +0200

    fix: case sensitive component imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
-import { Hero } from "../components/hero";
-import { Navbar } from "../components/navbar";
+import { Hero } from "@/components/hero";
+import { Navbar } from "@/components/navbar";
 
 export default function Home() {
   return (
